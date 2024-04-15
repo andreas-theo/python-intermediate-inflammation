0a00ecdd3cac4bd5c3a297b654e9df00bcbb0aff
Andreas Theodosiou <atheodosiou@protonmail.ch>
2024-04-15 12:18:41 +0200

    Add docstrings to the daily_mean, max, min functions

--- inflammation/models.py
+++ inflammation/models.py
@@ -19,16 +19,25 @@ def load_csv(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array.
+
+    :param data: An array containing the inflammation data.
+    :return: A daily mean of the inflammation data."""
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array.
+
+    :param data: An array containing the inflammation data.
+    :return: A daily max of the inflammation data."""
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
+    """Calculate the daily min of a 2D inflammation data array.
+
+    :param data: An array containing the inflammation data.
+    :return: A daily min of the inflammation data."""
     return np.min(data, axis=0)
 
