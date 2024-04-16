c95a443b4caf2593b53e685b8aed97843a80024c
Andreas Theodosiou <atheodosiou@protonmail.ch>
2024-04-16 13:44:20 +0200

    Add Observation, Person, and Patient classes

--- inflammation/models.py
+++ inflammation/models.py
@@ -7,7 +7,72 @@ inflammation data for a single patient taken over a number of days
 and each column represents a single day across all patients.
 """
 
+from functools import reduce
 import numpy as np
+
+
+class Observation:
+    def __init__(self, day, value):
+        self.day = day
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Person:
+    """A person."""
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+class Patient(Person):
+    """A patient in an inflammation study."""
+
+    def __init__(self, name):
+        super().__init__(name)
+        self.observations = []
+
+    def add_observation(self, value, day=None):
+        if day is None:
+            try:
+                day = self.observations[-1].day + 1
+
+            except IndexError:
+                day = 0
+
+        new_observation = Observation(day, value)
+
+        self.observations.append(new_observation)
+        return new_observation
+
+    def __str__(self):
+        return self.name
+
+
+class Doctor(Person):
+    """A doctor in an inflammation study."""
+
+    def __init__(self, name, patients):
+        super().__init__(name)
+        self.patients = patients
+
+    def add_patient(self, name):
+        if name in set(self.patients):
+            raise ValueError(f'Patient {name} already exists')
+        new_patient = Patient(name)
+        self.patients.append(new_patient)
+        return new_patient
+
+    def add_patients(self, names):
+        map(self.add_patient, names)
+
+    def __str__(self):
+        return self.name
 
 
 def load_csv(filename):
@@ -58,3 +123,24 @@ def patient_normalise(data):
     normalised[np.isnan(normalised)] = 0
     normalised[normalised < 0] = 0
     return normalised
+
+
+def daily_above_threshold(patient_row, data, threshold):
+    """Find the daily values that are above a threshold for a given patient.
+
+    :param patient_row: Row index corresponding to a patient.
+    :param data: An array containing the inflammation data.
+    :param threshold: Threshold for the patient.
+    :return: A list of booleans describing whether each value if above the threshold."""
+    above_threshold = map(lambda daily_value: daily_value > threshold, data[patient_row])
+    nr_above_threshold = reduce(lambda a, b: a + 1 if b else a, above_threshold, 0)
+    return nr_above_threshold
+
+
+def attach_names(patient_matrix, patient_names):
+    # Check that the rows in the matrix are as many as the names
+    assert patient_matrix.shape[0] == len(patient_names)
+    list_of_names_and_data = []
+    for name, data in zip(patient_names, patient_matrix):
+        list_of_names_and_data.append({'name': name, 'data': data})
+    return list_of_names_and_data
